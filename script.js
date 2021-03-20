82a7c00f1ffac7b2b701a92643382e1522bd715c
Bladerendeeg <41634346+Bladerendeeg@users.noreply.github.com>
2021-03-20 16:01:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const age = 4059;
+
+if (age < 18) {
+    console.log("Kom terug als je 18 bent");
+} else if (age > 90) {
+    console.log("Weet u het zeker dat u naar binnen wilt?");
+} else {
+    console.log("Kom binnen");
+}
+
+let isFemale = true;
+if (isFemale = true) {
+    console.log("Dag mevrouw");
+} else {
+    console.log("Nee nee. Jij bent niet welkom");
+}
+
+const driverStatus = "bob";
+if (driverStatus == "bob") {
+    console.log("Jij mag rijden");
+} else {
+    console.log("Dat wordt lopen!")
+}
